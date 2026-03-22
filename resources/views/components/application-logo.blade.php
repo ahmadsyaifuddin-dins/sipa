baeaaab6ff3f518a23c1a0096d2c31f2736fdfdb
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2026-03-22 20:06:10 +0800

    sip logo ditambah

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,9 +1,1 @@
-<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <path d="M3 21H21V7L12 3L3 7V21Z" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-        stroke-linejoin="round" />
-    <path d="M9 21V11H15V21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M15 7H19V15H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <path d="M5 15H9V7H5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-    <text x="12" y="16" text-anchor="middle" font-weight="900" font-size="5" fill="currentColor"
-        font-family="sans-serif">SIPA</text>
-</svg>
+<img src="{{ asset('logo.png') }}" alt="Logo Toko Azizah" {{ $attributes }}>
--- resources/views/components/pdf/_header.blade.php
+++ resources/views/components/pdf/_header.blade.php
@@ -2,8 +2,9 @@
     style="border-bottom: 3px solid #000; padding-bottom: 10px; margin-bottom: 20px; border-collapse: collapse;">
     <tr>
         <td width="15%" style="text-align: center; vertical-align: middle;">
-            <h1 style="margin: 0; color: #4338ca; font-size: 28px;">SIPA</h1>
+            <img src="{{ public_path('logo.png') }}" alt="Logo" style="max-width: 80px; height: auto;">
         </td>
+
         <td width="85%" style="text-align: center; vertical-align: middle;">
             <h2 style="margin: 0; font-size: 24px; text-transform: uppercase;">TOKO AZIZAH</h2>
             <h4 style="margin: 5px 0; font-size: 14px; font-weight: normal;">Sistem Informasi Penjualan ATK dan Layanan
